dedbab6cdc2308c87bd7fcebcf53e8fc547ecc57
Isaac Lal <isaaclal124@gmail.com>
2023-10-07 18:00:38 -0400

    Formatted website

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // variables and constants
 const cartContainer = document.querySelector('.cart-container');
 const productList = document.querySelector('.product-list');
@@ -10,43 +9,43 @@ let cartItemID = 1;
 eventListeners();
 
 // all event listeners
-function eventListeners(){
-    window.addEventListener('DOMContentLoaded', () => {
-        loadJSON();
-        loadCart();
-    });
-    // toggle navbar when toggle button is clicked
-    document.querySelector('.navbar-toggler').addEventListener('click', () => {
-        document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
-    });
+function eventListeners() {
+  window.addEventListener('DOMContentLoaded', () => {
+    loadJSON();
+    loadCart();
+  });
+  // toggle navbar when toggle button is clicked
+  document.querySelector('.navbar-toggler').addEventListener('click', () => {
+    document.querySelector('.navbar-collapse').classList.toggle('show-navbar');
+  });
 
-    // show/hide cart container
-    document.getElementById('cart-btn').addEventListener('click', () => {
-        cartContainer.classList.toggle('show-cart-container');
-    });
+  // show/hide cart container
+  document.getElementById('cart-btn').addEventListener('click', () => {
+    cartContainer.classList.toggle('show-cart-container');
+  });
 
-    // add to cart
-    productList.addEventListener('click', purchaseProduct);
+  // add to cart
+  productList.addEventListener('click', purchaseProduct);
 
-    // delete from cart
-    cartList.addEventListener('click', deleteProduct);
+  // delete from cart
+  cartList.addEventListener('click', deleteProduct);
 }
 
 // update cart info
-function updateCartInfo(){
-    let cartInfo = findCartInfo();
-    cartCountInfo.textContent = cartInfo.productCount;
-    cartTotalValue.textContent = cartInfo.total;
+function updateCartInfo() {
+  let cartInfo = findCartInfo();
+  cartCountInfo.textContent = cartInfo.productCount;
+  cartTotalValue.textContent = cartInfo.total;
 }
 
 // load product items content form JSON file
-function loadJSON(){
-    fetch('furniture.json')
+function loadJSON() {
+  fetch('furniture.json')
     .then(response => response.json())
-    .then(data =>{
-        let html = '';
-        data.forEach(product => {
-            html += `
+    .then(data => {
+      let html = '';
+      data.forEach(product => {
+        html += `
                 <div class = "product-item">
                     <div class = "product-img">
                         <img src = "${product.imgSrc}" alt = "product image">
@@ -62,44 +61,43 @@ function loadJSON(){
                     </div>
                 </div>
             `;
-        });
-        productList.innerHTML = html;
+      });
+      productList.innerHTML = html;
     })
     .catch(error => {
-        alert(`User live server or local server`);
-        //URL scheme must be "http" or "https" for CORS request. You need to be serving your index.html locally or have your site hosted on a live server somewhere for the Fetch API to work properly.
-    })
+      alert(`User live server or local server`);
+      //URL scheme must be "http" or "https" for CORS request. You need to be serving your index.html locally or have your site hosted on a live server somewhere for the Fetch API to work properly.
+    });
 }
 
-
 // purchase product
-function purchaseProduct(e){
-    if(e.target.classList.contains('add-to-cart-btn')){
-        let product = e.target.parentElement.parentElement;
-        getProductInfo(product);
-    }
+function purchaseProduct(e) {
+  if (e.target.classList.contains('add-to-cart-btn')) {
+    let product = e.target.parentElement.parentElement;
+    getProductInfo(product);
+  }
 }
 
 // get product info after add to cart button click
-function getProductInfo(product){
-    let productInfo = {
-        id: cartItemID,
-        imgSrc: product.querySelector('.product-img img').src,
-        name: product.querySelector('.product-name').textContent,
-        category: product.querySelector('.product-category').textContent,
-        price: product.querySelector('.product-price').textContent
-    }
-    cartItemID++;
-    addToCartList(productInfo);
-    saveProductInStorage(productInfo);
+function getProductInfo(product) {
+  let productInfo = {
+    id: cartItemID,
+    imgSrc: product.querySelector('.product-img img').src,
+    name: product.querySelector('.product-name').textContent,
+    category: product.querySelector('.product-category').textContent,
+    price: product.querySelector('.product-price').textContent,
+  };
+  cartItemID++;
+  addToCartList(productInfo);
+  saveProductInStorage(productInfo);
 }
 
 // add the selected product to the cart list
-function addToCartList(product){
-    const cartItem = document.createElement('div');
-    cartItem.classList.add('cart-item');
-    cartItem.setAttribute('data-id', `${product.id}`);
-    cartItem.innerHTML = `
+function addToCartList(product) {
+  const cartItem = document.createElement('div');
+  cartItem.classList.add('cart-item');
+  cartItem.setAttribute('data-id', `${product.id}`);
+  cartItem.innerHTML = `
         <img src = "${product.imgSrc}" alt = "product image">
         <div class = "cart-item-info">
             <h3 class = "cart-item-name">${product.name}</h3>
@@ -111,68 +109,70 @@ function addToCartList(product){
             <i class = "fas fa-times"></i>
         </button>
     `;
-    cartList.appendChild(cartItem);
+  cartList.appendChild(cartItem);
 }
 
 // save the product in the local storage
-function saveProductInStorage(item){
-    let products = getProductFromStorage();
-    products.push(item);
-    localStorage.setItem('products', JSON.stringify(products));
-    updateCartInfo();
+function saveProductInStorage(item) {
+  let products = getProductFromStorage();
+  products.push(item);
+  localStorage.setItem('products', JSON.stringify(products));
+  updateCartInfo();
 }
 
 // get all the products info if there is any in the local storage
-function getProductFromStorage(){
-    return localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : [];
-    // returns empty array if there isn't any product info
+function getProductFromStorage() {
+  return localStorage.getItem('products')
+    ? JSON.parse(localStorage.getItem('products'))
+    : [];
+  // returns empty array if there isn't any product info
 }
 
 // load carts product
-function loadCart(){
-    let products = getProductFromStorage();
-    if(products.length < 1){
-        cartItemID = 1; // if there is no any product in the local storage
-    } else {
-        cartItemID = products[products.length - 1].id;
-        cartItemID++;
-        // else get the id of the last product and increase it by 1
-    }
-    products.forEach(product => addToCartList(product));
+function loadCart() {
+  let products = getProductFromStorage();
+  if (products.length < 1) {
+    cartItemID = 1; // if there is no any product in the local storage
+  } else {
+    cartItemID = products[products.length - 1].id;
+    cartItemID++;
+    // else get the id of the last product and increase it by 1
+  }
+  products.forEach(product => addToCartList(product));
 
-    // calculate and update UI of cart info 
-    updateCartInfo();
+  // calculate and update UI of cart info
+  updateCartInfo();
 }
 
 // calculate total price of the cart and other info
-function findCartInfo(){
-    let products = getProductFromStorage();
-    let total = products.reduce((acc, product) => {
-        let price = parseFloat(product.price.substr(1)); // removing dollar sign
-        return acc += price;
-    }, 0); // adding all the prices
+function findCartInfo() {
+  let products = getProductFromStorage();
+  let total = products.reduce((acc, product) => {
+    let price = parseFloat(product.price.substr(1)); // removing dollar sign
+    return (acc += price);
+  }, 0); // adding all the prices
 
-    return{
-        total: total.toFixed(2),
-        productCount: products.length
-    }
+  return {
+    total: total.toFixed(2),
+    productCount: products.length,
+  };
 }
 
 // delete product from cart list and local storage
-function deleteProduct(e){
-    let cartItem;
-    if(e.target.tagName === "BUTTON"){
-        cartItem = e.target.parentElement;
-        cartItem.remove(); // this removes from the DOM only
-    } else if(e.target.tagName === "I"){
-        cartItem = e.target.parentElement.parentElement;
-        cartItem.remove(); // this removes from the DOM only
-    }
+function deleteProduct(e) {
+  let cartItem;
+  if (e.target.tagName === 'BUTTON') {
+    cartItem = e.target.parentElement;
+    cartItem.remove(); // this removes from the DOM only
+  } else if (e.target.tagName === 'I') {
+    cartItem = e.target.parentElement.parentElement;
+    cartItem.remove(); // this removes from the DOM only
+  }
 
-    let products = getProductFromStorage();
-    let updatedProducts = products.filter(product => {
-        return product.id !== parseInt(cartItem.dataset.id);
-    });
-    localStorage.setItem('products', JSON.stringify(updatedProducts)); // updating the product list after the deletion
-    updateCartInfo();
+  let products = getProductFromStorage();
+  let updatedProducts = products.filter(product => {
+    return product.id !== parseInt(cartItem.dataset.id);
+  });
+  localStorage.setItem('products', JSON.stringify(updatedProducts)); // updating the product list after the deletion
+  updateCartInfo();
 }
